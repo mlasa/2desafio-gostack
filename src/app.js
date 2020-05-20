d76ef2bb99f5edd58d9ea5926c6215e272d4b887
Marcella LASA <marcella.amorimsa@gmail.com>
2020-05-20 15:32:34 -0300

    CRUD and likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,65 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+  repositories.push(repo);
+
+  return response.status(200).json(repo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  let repoIndex = repositories.findIndex((r) => r.id === id);
+  if (repoIndex < false) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  // repositories[repoIndex] = {
+  //   title: title,
+  //   url: url,
+  //   techs: techs,
+  // };
+  repositories[repoIndex]  = {...repositories[repoIndex],title,url,techs}
+
+  return response.status(200).json(repositories[repoIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  let repoIndex = repositories.findIndex((r) => r.id === id);
+  if (repoIndex < false) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repo = repositories.find((r) => r.id === id);
+  if (!repo) {
+    return response.status(400).json({ error: "Repository not found" });
+  }
+  repo.likes++;
+
+  return response.status(200).json(repo);
 });
 
 module.exports = app;
